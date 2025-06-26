4668e9a6c35d35144166592a40123105b2477bc2
buiduclinh <builinh120396@gmail.com>
2025-06-26 18:45:17 +0700

    Add files via upload

--- TPLUS_JV250321_MD04_Session04/TPLUS_JV250321_MD04_Session04_bai5/src/Main.java
+++ TPLUS_JV250321_MD04_Session04/TPLUS_JV250321_MD04_Session04_bai5/src/Main.java
@@ -4,9 +4,27 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String str = sc.nextLine();
-        StringBuffer sbf = new StringBuffer();
-        StringBuilder sbd = new StringBuilder();
+        int number = 100;
+        long start = System.currentTimeMillis();
+        String s = "hello";
+        for (int i = 0; i < number; i++) {
+            s += "world";
+        }
+        long str = System.currentTimeMillis() -  start;
+        System.out.println(str);
+        start = System.currentTimeMillis();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < number; i++) {
+            sb.append("world");
+        }
+        long str2 = System.currentTimeMillis() -  start;
+        System.out.println(str2);
+        start = System.currentTimeMillis();
+        StringBuffer sb2 = new StringBuffer();
+        for (int i = 0; i < number; i++) {
+            sb2.append("world");
+        }
+        long str3 = System.currentTimeMillis() -  start;
+        System.out.println(str3);
     }
 }
